003af9bb921cbc91a8e52f128e164030f0d67a22
Estevan Gladstone <estevangladstone@ejcm.com.br>
2016-06-16 23:17:46 -0300

    Tentandntando colocar as tampas no cilindro, mas falhando

--- RayTrace/projeto/src/Cilindro.cpp
+++ RayTrace/projeto/src/Cilindro.cpp
@@ -13,7 +13,7 @@ Cilindro::Cilindro(int _indice_textura, float _raio, float _altura, const Ponto_
 Intersection Cilindro::Intercepta(const Raio& r_vis, IntersectionMode mode, float threshold)
 {
   float a, b, c, delta, hmin, hmax, t1, t2, y1, y2;
-  Intersection intersection;
+  Intersection intersection, plano, sides;
 
   // valores intermediários
   Vetor_3D K = Vetor_3D(r_vis.X0() - centro.X(),
@@ -32,20 +32,60 @@ Intersection Cilindro::Intercepta(const Raio& r_vis, IntersectionMode mode, floa
 
   delta = b*b - 4*a*c;
 
+  Vetor_3D pontoNoPlanoSuperior(centro.X(), hmax, centro.Z());
+  Vetor_3D pontoNoPlanoInferior(centro.X(), hmin, centro.Z());
+  Vetor_3D normalAosPlanos(0, 1, 0); // A base do cilindro é paralela ao plano XZ
+
+  float naoParalelos = r_vis.Direcao().produtoEscalar(normalAosPlanos);
 
   if (delta >= 0) {
       t1=(-b - sqrt(delta)) /(2*a);
       t2=(-b + sqrt(delta)) /(2*a);
       y1=K.Y()+t1*r_vis.Dy();
       y2=K.Y()+t2*r_vis.Dy();
-      if(((y1>hmin)&&(y1<hmax))&&((y2<hmin)||(y2>hmax))){
+      if(((y1 >= hmin)&&(y1 <= hmax)) && ((y2 < hmin)||(y2 > hmax))){
         intersection = Intersection(this, t1);
-      } else if(((y1<hmin)||(y1>hmax))&&((y2>hmin)&&(y2<hmax))){
-        intersection = Intersection(this, t2);
+//        if(naoParalelos){
+//          float p1 = normalAosPlanos.produtoEscalar(Vetor_3D(pontoNoPlanoInferior - Vetor_3D(r_vis.Origem().X(), r_vis.Origem().Y(), r_vis.Origem().Z())))/naoParalelos;
+//          float p2 = normalAosPlanos.produtoEscalar(Vetor_3D(pontoNoPlanoSuperior - Vetor_3D(r_vis.Origem().X(), r_vis.Origem().Y(), r_vis.Origem().Z())))/naoParalelos;
+//          plano = Intersection::nearest(
+//                      Intersection(this, p1),
+//                      Intersection(this, p2),  threshold);
+//          intersection = Intersection::nearest(
+//                      plano,
+//                      Intersection(this, t1),  threshold);
+//        }
+      } else if(((y1 <= hmin)||(y1 >= hmax))&&((y2 > hmin)&&(y2 < hmax))){
+          intersection = Intersection(this, t2);
+//        if(naoParalelos){
+//          float p1 = normalAosPlanos.produtoEscalar(Vetor_3D(pontoNoPlanoInferior - Vetor_3D(r_vis.Origem().X(), r_vis.Origem().Y(), r_vis.Origem().Z())))/naoParalelos;
+//          float p2 = normalAosPlanos.produtoEscalar(Vetor_3D(pontoNoPlanoSuperior - Vetor_3D(r_vis.Origem().X(), r_vis.Origem().Y(), r_vis.Origem().Z())))/naoParalelos;
+//          plano = Intersection::nearest(
+//                      Intersection(this, p1),
+//                      Intersection(this, p2),  threshold);
+//          intersection = Intersection::nearest(
+//                      plano,
+//                      Intersection(this, t1),  threshold);
+//        }
       } else if (((y1>hmin)&&(y1<hmax))&&((y2>hmin)&&(y2<hmax))){
-          intersection = Intersection::nearest(
-                      Intersection(this, t1),
-                      Intersection(this, t2),  threshold);
+//          if(naoParalelos){
+//            float p1 = normalAosPlanos.produtoEscalar(Vetor_3D(pontoNoPlanoInferior - Vetor_3D(r_vis.Origem().X(), r_vis.Origem().Y(), r_vis.Origem().Z())))/naoParalelos;
+//            float p2 = normalAosPlanos.produtoEscalar(Vetor_3D(pontoNoPlanoSuperior - Vetor_3D(r_vis.Origem().X(), r_vis.Origem().Y(), r_vis.Origem().Z())))/naoParalelos;
+//            plano = Intersection::nearest(
+//                        Intersection(this, p1),
+//                        Intersection(this, p2),  threshold);
+//            sides = Intersection::nearest(
+//                        Intersection(this, t2),
+//                        Intersection(this, t1),  threshold);
+//            intersection = Intersection::nearest(
+//                        plano,
+//                        sides,  threshold);
+//          }
+//          else{
+              intersection = Intersection::nearest(
+                          Intersection(this, t1),
+                          Intersection(this, t2),  threshold);
+//          }
       }
   }
 
@@ -54,11 +94,17 @@ Intersection Cilindro::Intercepta(const Raio& r_vis, IntersectionMode mode, floa
 
 Vetor_3D Cilindro::normal( const Ponto_3D& ponto ) const
 {
-    Vetor_3D tmp(ponto - centro);
-    Vetor_3D centroNaAlturaDoPonto =  Vetor_3D(centro.X(), ponto.Y(), centro.Z());
+    Vetor_3D tmp;
 
-    tmp = ponto - centroNaAlturaDoPonto;
-    tmp.normaliza();
+//    if(ponto.Y() == (centro.Y() + altura/2)){
+//        tmp = Vetor_3D(0, 1, 0);
+//    } else if(ponto.Y() == (centro.Y() - altura/2)) {
+//        tmp = Vetor_3D(0, -1, 0);
+//    } else {
+        Vetor_3D centroNaAlturaDoPonto(centro.X(), ponto.Y(), centro.Z());
+        tmp = ponto - centroNaAlturaDoPonto;
+        tmp.normaliza();
+//    }
 
     return tmp;
 }
